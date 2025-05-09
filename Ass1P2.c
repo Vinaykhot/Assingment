e174c170a1c68c358f1d40084642e2bd8fd91e3b
Vinaykhot <vinaykhot42002@gmail.com>
2025-05-10 01:49:10 +0530

    Create Ass1P2.c

---
+++ Ass1P2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void Display()
+{
+    int i=0;
+    for(i=1;i<=5;i++)
+    {
+        printf("Marvellous Infosystem\n");
+    }
+}
+int main()
+{
+    Display();
+    return 0;
+}
